b138cc6cb5006f216741e02b00fd7d45f1449936
wexleysilva <101215059+wexleysilva@users.noreply.github.com>
2023-08-29 11:48:59 -0300

    Create Veiculo.java

---
+++ Veiculo.java
@@ -0,0 +1,42 @@
+package Modelo;
+
+public class Veiculo {
+
+	
+	private String modelo;
+	private Integer velocidadeMax;
+	private Integer qtdPassageiros;
+	private String cor;
+	
+	
+	
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public Integer getVelocidadeMax() {
+		return velocidadeMax;
+	}
+	public void setVelocidadeMax(Integer velocidadeMax) {
+		this.velocidadeMax = velocidadeMax;
+	}
+	public Integer getQtdPassageiros() {
+		return qtdPassageiros;
+	}
+	public void setQtdPassageiros(Integer qtdPassageiros) {
+		this.qtdPassageiros = qtdPassageiros;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+
+
+}
+
+
